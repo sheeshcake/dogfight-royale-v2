17971e71c80a8c346ed512122802c701b4c356bf
wendale.dy <spooderm5@gmail.com>
2022-09-02 10:32:29 +0800

    update added networking

---
+++ scripts/network_setup.gd
@@ -0,0 +1,50 @@
+extends Control
+
+
+onready var multiplayer_config_ui = $multiplayer_config
+onready var server_ip_address = $multiplayer_config/server_ip_address
+onready var device_ip_address = $CanvasLayer/Label
+
+var player = load('res://node/player_with_plane.tscn')
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_tree().connect("network_peer_connected", self, '_player_connected')
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_connected_to_server")
+	device_ip_address.text = Network.ip_address
+
+func _player_connected(id):
+	print('player: ' + id + " connected")
+	instance_player(id)
+
+func _player_disconnected(id):
+	print('player: ' + id + " disconnected")
+	if Players.has_node(id):
+		Players.get_node(id).queue_free()
+
+
+func _on_create_server_pressed():
+	multiplayer_config_ui.hide()
+	Network.create_server()
+	
+	instance_player(get_tree().get_network_unique_id())
+	
+
+func _connected_to_server():
+	yield(get_tree().create_timer(0.1), 'timeout')
+	instance_player(get_tree().get_network_unique_id())
+
+
+func _on_join_server_pressed():
+	if server_ip_address.text != "":
+		multiplayer_config_ui.hide()
+		Network.ip_address = server_ip_address.text
+		Network.join_server()
+		
+
+func instance_player(id):
+	var player_instance = Global.instance_node_at_location(player, Players, Vector2(rand_range(0, 1280), rand_range(0, 720)))
+	player_instance.name = str(id)
+	player_instance.set_network_master(id)
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,11 +5,15 @@ const BULLET = preload("res://node/bullet.tscn")
 export (int) var speed = 50
 export (float) var rotation_speed = 2
 
-var velocity = Vector2()
+var velocity = Vector2(0, 0)
 var rotation_dir = 0
 var running = false
 var ready_to_shoot = true
 var on_floor = true
+onready var tween = $Tween
+puppet var puppet_position = Vector2(0, 0) setget puppet_position_set
+puppet var puppet_rotation = 0
+puppet var puppet_velocity = Vector2()
 
 func start(pos):
 	velocity = Vector2(pos)
@@ -39,10 +43,26 @@ func shoot():
 		
 
 func _physics_process(delta):
-	get_input()
-	rotation += rotation_dir * rotation_speed * delta
-	velocity = Vector2(speed, 0).rotated(rotation)
+	if is_network_master():
+		get_input()
+		rotation += rotation_dir * rotation_speed * delta
+		velocity = Vector2(speed, 0).rotated(rotation)
+	else:
+		rotation_degrees = lerp(rotation_degrees, puppet_rotation, delta * 8)
+		if not tween.is_active():
+			puppet_velocity = Vector2(speed, 0).rotated(rotation)
 
+func puppet_position_set(new_value):
+	puppet_position = new_value
+	tween.interpolate_property(self, "global_position", global_position, 0.1)
+	tween.start()
 
 func _on_Timer_timeout():
 	ready_to_shoot = true
+
+
+func _on_NetworkTickRate_timeout():
+	if is_network_master():
+		rset_unreliable('puppet_position', global_position)
+		rset_unreliable('puppet_velocity', velocity)
+		rset_unreliable('puppet_rotation', rotation_degrees)
